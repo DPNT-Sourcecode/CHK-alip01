9682353abb3cbde34bcfb077c70922f9cbbc9d6e
sbx_user1060 <sbx_user1060@ip-10-129-2-63.eu-west-2.compute.internal>
2019-01-16 12:08:21 +0000

    Wed Jan 16 12:08:21 UTC 2019

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,12 +1,36 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+        public class PriceRule
+        {
+            public string Item;
+            public int Amount;
+            public int Price;
+            public PriceRule(string item, int amount, int price)
+            {
+                Item = item;
+                Amount = amount;
+                Price = price;
+            }
+        }
+        public static List<PriceRule> rules = new List<PriceRule>()
+        {
+            new PriceRule("A", 3, 130),
+            new PriceRule("A", 1, 50),
+            new PriceRule("B", 2, 45),
+            new PriceRule("B", 1, 30),
+            new PriceRule("C", 1, 20),
+            new PriceRule("D", 1, 15),
+        };
+            
         public static int Checkout(string skus)
         {
             throw new SolutionNotImplementedException();
         }
     }
 }
+
